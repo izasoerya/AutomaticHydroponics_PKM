8ed951d0d9a3e8362dd0da0f679ba56cdf318e80
izasoerya <ihza.surya@gmail.com>
2023-12-28 00:01:44 +0700

    add rtc auto calibration using __TIME__

--- src/rtc.cpp
+++ src/rtc.cpp
@@ -1,23 +1,23 @@
 #include <Arduino.h>
 #include "rtc.h"
 
-RTC::RTC():myRTC(RTC_DS3231()){}
+RTC::RTC() : myRTC(RTC_DS3231()) {}
 
 void RTC::begin()
 {
     Wire.begin();
     myRTC.begin();
-    // myRTC.adjust(DateTime(2023, 11, 19, 22, 55, 00));
+    if (myRTC.lostPower())
+        myRTC.adjust(DateTime(F(__DATE__), F(__TIME__)));
 }
 
 void RTC::fetchData(myTime &time)
 {
     DateTime now = myRTC.now();
-    time.day    = now.day(), DEC;
-    time.month  = now.month(), DEC;
-    time.year   = now.year(), DEC;
+    time.day = now.day(), DEC;
+    time.month = now.month(), DEC;
+    time.year = now.year(), DEC;
     time.second = now.second(), DEC;
     time.minute = now.minute(), DEC;
-    time.hour   = now.hour(), DEC;
+    time.hour = now.hour(), DEC;
 }
-
